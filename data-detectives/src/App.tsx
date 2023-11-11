c05acf787c166167f0c85ab5f336a0f838b476e1
suzybee1987 <suzannemiller1987@gmail.com>
2023-11-11 12:47:14 +0000

    feature: add postcode input and onblur event listener

--- data-detectives/src/App.tsx
+++ data-detectives/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Topics } from "./Topics";
+import { PostCodeFinder } from "./Postcode";
 
 const App = () => {
   return (
     <div className="App App-header">
       <header>Data Detectives</header>
+      <PostCodeFinder />
       <Topics />
     </div>
   );

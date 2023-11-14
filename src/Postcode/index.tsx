8252704515684c05efab2ef60ca7a278ae47f843
suzybee1987 <suzannemiller1987@gmail.com>
2023-11-14 10:50:54 +0000

    feature: hook up api proxy

--- src/Postcode/index.tsx
+++ src/Postcode/index.tsx
@@ -1,6 +1,32 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useState } from "react";
 
 export const PostCodeFinder: FC = (): React.ReactElement => {
+  const [inspectionData, setInspectionData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Replace the URL with your API endpoint
+
+        const apiUrl =
+          "/api/get-inspection-data?code=7dmx_V2A6ZgyAlXiy1lRFZ415KNL1UsYj1b1QnNjcGydAzFuPdygKQ==&&columnName=UPRN&&columnValue=10012142007";
+
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        setInspectionData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array means this effect will only run once, similar to componentDidMount
+
   useEffect(() => {
     const inputElement = document.getElementById("input") as HTMLInputElement;
 
@@ -47,6 +73,13 @@ export const PostCodeFinder: FC = (): React.ReactElement => {
     <div className="m-3">
       <h2 className="small">Please enter your preferred post code:</h2>
       <input id="input" type="text" required />
+      {inspectionData ? (
+        // Render your data here
+        <pre>{JSON.stringify(inspectionData, null, 2)}</pre>
+      ) : (
+        // Loading state or error handling
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
